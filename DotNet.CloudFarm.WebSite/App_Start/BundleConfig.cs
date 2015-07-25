c1dc52ddd6756790ef659b3bcd49d6d4fe0d1cee
hyq1860 <pchero@163.com>
2015-07-25 09:02:06 +0800

    jquery引用压缩版本

--- DotNet.CloudFarm.WebSite/App_Start/BundleConfig.cs
+++ DotNet.CloudFarm.WebSite/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace DotNet.CloudFarm.WebSite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-2.1.4.min.js",
                         "~/Scripts/common.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
